be8b144f13cdd9512d91fb53b97bf9634593f350
Marcus Geiger <git013@teufelsgraben.xyz>
2021-11-05 00:30:05 +0100

    Build container image using Nix

--- flake.nix
+++ flake.nix
@@ -11,14 +11,45 @@
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
 
+        # What we build and the command.
+        the-name = "gandi-dns-update";
+
         rust = pkgs.rust-bin.stable."1.56.1";
         rust-bin = rust.default;
         rust-src = rust.rust-src;
+
+        myRustPlatform = pkgs.makeRustPlatform {
+          cargo = rust-bin;
+          rustc = rust-bin;
+        };
+
+        cargo-version = (builtins.fromTOML (builtins.readFile ./Cargo.toml) ).package.version;
+
+        gandi-dns-update = myRustPlatform.buildRustPackage rec {
+          pname = the-name;
+          version = cargo-version;
+          src = pkgs.lib.cleanSource ./.;
+          cargoLock = { lockFile = ./Cargo.lock; };
+          dockCheck = false; # Disable tests.
+        };
       in {
+        defaultPackage = gandi-dns-update;
+
+        # Nix: nix build .#gandi-dns-update-image && docker load < result
+        packages.gandi-dns-update-image = pkgs.dockerTools.buildLayeredImage {
+          name = the-name;
+          tag = cargo-version;
+          contents = [ pkgs.cacert gandi-dns-update ];
+          config = {
+            Cmd = [ the-name ];
+            WorkingDir = "/";
+          };
+        };
+
         devShell = pkgs.mkShell {
           buildInputs = [
-            rust.default
-            rust.rust-src
+            rust-bin
+            rust-src
             pkgs.rust-analyzer
           ];
 
